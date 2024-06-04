f0ef239d7ee53ddceb3ab42cd9f73e5c141cae86
Paula Beer <paula_beer@poli.ufrj.br>
2024-06-04 18:07:56 +0000

    Game development with TDD

---
+++ longest_word/game.py
@@ -0,0 +1,19 @@
+import random
+import string
+
+class Game:
+    def __init__(self) -> list:
+        """Attribute a random grid to size 9"""
+        self.grid = [random.choice(string.ascii_uppercase) for _ in range(9)]
+
+    def is_valid(self, word: str) -> bool:
+        """Return True if and only if the word is valid, given the Game's grid"""
+        if not word:
+            return False
+        letters = self.grid.copy() # Consume letters from the grid
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+        return True
