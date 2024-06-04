f08521f642a1cecfa5d181aefb4869ccfbb1496c
Paula Beer <paula_beer@poli.ufrj.br>
2024-06-04 19:21:23 +0000

    Small web app wrapper around the Game

--- wsgi.py
+++ wsgi.py
@@ -9,7 +9,7 @@
 #     return "Hello world!"
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from longest_word.game import Game
 
 app = Flask(__name__)
@@ -18,3 +18,11 @@ app = Flask(__name__)
 def home():
     game = Game()
     return render_template('home.html', grid=game.grid)
+
+@app.route('/check', methods=["POST"])
+def check():
+    game = Game()
+    game.grid = list(request.form['grid'])
+    word = request.form['word']
+    is_valid = game.is_valid(word)
+    return render_template('check.html', is_valid=is_valid, grid=game.grid, word=word)
