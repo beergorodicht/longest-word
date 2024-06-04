040ed10504bb41122651c71a990bbea61351d9c0
Paula Beer <paula_beer@poli.ufrj.br>
2024-06-04 18:30:03 +0000

    Feature complete: Dictionary check of attempt

--- longest_word/game.py
+++ longest_word/game.py
@@ -1,5 +1,7 @@
 import random
 import string
+import requests
+
 
 class Game:
     def __init__(self) -> list:
@@ -8,12 +10,23 @@ class Game:
 
     def is_valid(self, word: str) -> bool:
         """Return True if and only if the word is valid, given the Game's grid"""
-        if not word:
-            return False
-        letters = self.grid.copy() # Consume letters from the grid
-        for letter in word:
-            if letter in letters:
-                letters.remove(letter)
-            else:
-                return False
-        return True
+        # if not word:
+        #     return False
+        # letters = self.grid.copy() # Consume letters from the grid
+        # for letter in word:
+        #     if letter in letters:
+        #         letters.remove(letter)
+        #     else:
+        #         return False
+        # return True
+        return self.__check_dictionary(word)
+
+    # In Python, a @staticmethod decorator is used to define a static method within a class.
+    # A static method is a method that belongs to the class rather than an instance of the class.
+    # It does not require access to the instance (self) or class (cls) itself, which means it cannot modify the state of the instance or class.
+
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://dictionary.lewagon.com/{word}")
+        json_response = response.json()
+        return json_response['found']
